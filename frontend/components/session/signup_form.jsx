a0f586f6bdd674bb839e2ce40cf08d676c61ee59
andy <jia470079834@gmail.com>
2021-05-28 23:04:25 -0400

    signup form

---
+++ frontend/components/session/signup_form.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createNewUser } from '../../actions/session_actions';
+
+const mSTP = ({errors}) => ({
+    errors: errors,
+    formType: 'Create Account',
+    navLink: <Link to="/login" className="" >Log in</Link>
+});
+
+const mDTP = dispatch => {
+    return {
+        createNewUser: formUser => dispatch(createNewUser(formUser))
+    }
+};
+
+class SignUpForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.update = this.update.bind(this);
+    }
+
+    update(field){
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.createNewUser(Object.assign({}, this.state))
+    }
+
+    render(){
+        return (
+            <form className="" onSubmit={this.handleSubmit}>
+                <p>hello</p>
+            </form>
+        )
+    }
+}

b9d4bc4c47ea771a693d956384a84054dc7f1de9
andy <jia470079834@gmail.com>
2021-05-29 15:25:57 -0400

    session show

---
+++ frontend/components/home/session_show.jsx
@@ -0,0 +1,27 @@
+import { fromPairs } from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { openModal } from '../../actions/modal_actions';
+
+const mSTP = state => ({
+    currentUser: state.session.currentUser
+});
+
+const mDTP = dispatch => ({
+    openModal: modal => dispatch(openModal(modal))
+});
+
+const sessionShow = ({ currentUser, openModal}) => {
+    const sessionLinks = () => (
+        <Link to="login">Log In</Link>
+    )
+    const greeting = () => (
+        <a onClick={() => openModal('profileDropDown')}>
+            hello
+        </a>
+    )
+    return currentUser ? greeting() : sessionLinks();
+}
+
+export default connect(mSTP, mDTP)(sessionShow);

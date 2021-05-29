b686c8d3efca1c69c8a255ed539525c4f8678c27
andy <jia470079834@gmail.com>
2021-05-29 15:15:41 -0400

    setup profile dropdown

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { closeModal } from '../../actions/modal_actions';
 import { logout } from '../../actions/session_actions';
-import ProfileDropDown from ''
+import ProfileDropDown from '../users/profile_dropdown';
 const mSTP = state => {
     return {
         modal: state.ui.modal
---
+++ frontend/components/users/profile_dropdown.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import { closeModal } from '../../actions/modal_actions';
+
+const mSTP = state => {
+    return {
+        currentUser: state.session.currentUser
+    }
+}
+
+const mDTP = dispatch => ({
+    logout: () => dispatch(logout()),
+    closeModal: () => dispatch(closeModal()),
+})
+
+class ProfileDropDown extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e){
+        e.preventDefault();
+        this.props.logout().then(this.props.closeModal());
+        window.location.replace("/")
+    }
+
+    render(){
+        const {currentUser} = this.props
+        return (
+            <div className="">
+                <button className="" onClick={this.handleClick}>logout</button>
+            </div>
+        )
+    }
+}
+
+export default connect(mSTP, mDTP)(ProfileDropDown);

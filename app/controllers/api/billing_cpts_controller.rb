b74438b4caddac594f7c9820769b8d07d3dba2d5
andy <jia470079834@gmail.com>
2021-06-06 23:52:08 -0400

    fixed billing cpt & icd controller

--- app/controllers/api/billing_cpts_controller.rb
+++ app/controllers/api/billing_cpts_controller.rb
@@ -1,3 +1,55 @@
 class Api::BillingCptsController < ApplicationController
-    
+    before_action :ensure_logged_in
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @billing_cpts = BillingCpt.all.select{|billing_cpt| billing_cpt.claim_id == }
+        render :index
+    end
+
+    def show
+        @billing_cpt = BillingCpt.find(params[:id])
+        render :show
+    end
+
+    def new
+        @billing_cpt = BillingCpt.new
+    end
+
+    def edit
+        @billing_cpt = BillingCpt.find(params[:id])
+    end
+
+    def create
+        @billing_cpt = BillingCpt.new(billing_cpt_params)
+        if @billing_cpt.save
+            render :show
+        else
+            render json: @billing_cpt.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @billing_cpt = BillingCpt.find(params[:billing_cpt][:id])
+        if @billing_cpt.update(billing_cpt_params)
+            render :show
+        else
+            render json: @billing_cpt.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @billing_cpt = billing_cpt.find(params[:id])
+        if @billing_cpt
+            @billing_cpt.destroy
+        end
+    end
+
+    def billing_cpt_params
+        params.require(:billing_cpt).permit(
+            :billing_cpt_code,
+            :billing_cpt_description,
+            :billed_amount
+        )
+    end
 end
--- app/controllers/api/billing_icds_controller.rb
+++ app/controllers/api/billing_icds_controller.rb
@@ -3,52 +3,52 @@ class Api::BillingIcdsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        @billing_cpts = BillingCpt.all
+        @billing_icds = BillingCpt.all.select{|billing_icd| billing_icd.claim_id == }
         render :index
     end
 
     def show
-        @billing_cpt = BillingCpt.find(params[:id])
+        @billing_icd = BillingCpt.find(params[:id])
         render :show
     end
 
     def new
-        @billing_cpt = BillingCpt.new
+        @billing_icd = BillingCpt.new
     end
 
     def edit
-        @billing_cpt = BillingCpt.find(params[:id])
+        @billing_icd = BillingCpt.find(params[:id])
     end
 
     def create
-        @billing_cpt = BillingCpt.new(billing_cpt_params)
-        if @billing_cpt.save
+        @billing_icd = BillingCpt.new(billing_icd_params)
+        if @billing_icd.save
             render :show
         else
-            render json: @billing_cpt.errors.full_messages, status: 422
+            render json: @billing_icd.errors.full_messages, status: 422
         end
     end
 
     def update
-        @billing_cpt = BillingCpt.find(params[:billing_cpt][:id])
-        if @billing_cpt.update(billing_cpt_params)
+        @billing_icd = BillingCpt.find(params[:billing_icd][:id])
+        if @billing_icd.update(billing_icd_params)
             render :show
         else
-            render json: @billing_cpt.errors.full_messages, status: 422
+            render json: @billing_icd.errors.full_messages, status: 422
         end
     end
 
     def destroy
-        @billing_cpt = billing_cpt.find(params[:id])
-        if @billing_cpt
-            @billing_cpt.destroy
+        @billing_icd = billing_icd.find(params[:id])
+        if @billing_icd
+            @billing_icd.destroy
         end
     end
 
-    def billing_cpt_params
-        params.require(:billing_cpt).permit(
-            :billing_cpt_code,
-            :billing_cpt_description,
+    def billing_icd_params
+        params.require(:billing_icd).permit(
+            :billing_icd_code,
+            :billing_icd_description,
             :billed_amount
         )
     end

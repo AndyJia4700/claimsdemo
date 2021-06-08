ad1d21cf1476dbddab41b35ff70964f03e73192e
andy <jia470079834@gmail.com>
2021-06-08 00:30:39 -0400

    update entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import userReducer from './users_reducer';
 import patientReducer from './patients_reducer';
 import claimReducer from './claims_reducer';
+import cptReducer from './cpts_reducer';
+import icdReducer from './icds_reducer';
 
 const entitiesReducer = combineReducers({
     user: userReducer,
     patient: patientReducer,
-    claim: claimReducer
+    claim: claimReducer,
+    cpt: cptReducer,
+    icd: icdReducer, 
 });
 
 export default entitiesReducer;

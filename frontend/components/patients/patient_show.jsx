c2a8d983a7cd274342012b5d967b86445ea0b50c
andy <jia470079834@gmail.com>
2021-06-07 23:58:51 -0400

    patient show with claim styling

--- frontend/components/patients/patient_show.jsx
+++ frontend/components/patients/patient_show.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPatient, deletePatient } from '../../actions/patient_actions';
 import { fetchClaims } from '../../actions/claim_actions';
-import {FaEdit} from 'react-icons/fa';
+import {FaEdit, FaPlus} from 'react-icons/fa';
+import {FcProcess, FcApproval} from 'react-icons/fc';
 import PatientIndex from './patient_index';
 
 const mSTP = (state, ownProps) => {
@@ -43,6 +44,7 @@ class PatientShow extends React.Component{
     
     render(){
         if (!this.props.patient) return null;
+        const dateFormat = require("dateformat");
 
         const {
             patient
@@ -50,10 +52,18 @@ class PatientShow extends React.Component{
 
         const claimList = Object.values(this.props.claims).map( claim => 
             claim.patient_id == this.props.patient.id ?
-            <li key={claim.id} className="">
-                {claim.claim_date_of_service}
-                {claim.claim_number}
-                {claim.message}
+            <li key={claim.id} className="patient-claim-index-ul-li">
+                <span className="patient-claim-index-ul-li-span1">
+                    {dateFormat(claim.claim_date_of_service, "mm/dd/yyyy")}
+                </span>
+
+                <span className="patient-claim-index-ul-li-span2">
+                    {claim.claim_number}
+                </span>
+
+                <span className="patient-claim-index-ul-li-span3">
+                    {claim.message == "pending" ? <FcProcess/> : <FcApproval/>}
+                </span>
             </li> : null
         )
         
@@ -62,29 +72,54 @@ class PatientShow extends React.Component{
                 <PatientIndex/>
 
                 <div className="patient-info-div">
+
                     <div className="patient-show-div">
-                        <label className="patient-show-element-title">Member: </label>
-                        <p className="patient-show-element">
-                            {patient.name}
-                        </p>
+                        <div className="patient-show-div-wrap">
+                            <div className="patient-show-div-sub">
+                                <label className="patient-show-element-title">Member: </label>
+                                <p className="patient-show-element">
+                                    {patient.name}
+                                </p>
+                            </div>
+                            
+                            <div className="patient-show-div-sub">
+                                <label className="patient-show-element-title">DOB: </label>
+                                <p className="patient-show-element">
+                                    {dateFormat(patient.birthdate, 'mm/dd/yyyy')} 
+                                </p>
+                            </div>
 
-                        <label className="patient-show-element-title">DOB: </label>
-                        <p className="patient-show-element">
-                            {patient.birthdate} 
-                        </p>
+                            <div className="patient-show-div-sub">
+                                <label className="patient-show-element-title">Insurance: </label>
+                                <p className="patient-show-element">
+                                    {patient.insurance_id} 
+                                </p>
+                            </div>
+                        </div>
 
-
-                        <label className="patient-show-element-title">Insurance ID: </label>
-                        <p className="patient-show-element">
-                            {patient.insurance_id} 
-                        </p>
-
-                        <span className="patient-show-element-edit" onClick={()=>window.location.replace(`#/patients/${patient.id}/edit`)}><FaEdit/></span>
+                        <span className="patient-show-element-edit" onClick={()=>window.location.replace(`#/patients/${patient.id}/edit`)}>
+                            <FaEdit/>
+                        </span>
                     </div>
 
-                    <ul className="patient-claim-index-div">
-                        {claimList}
-                    </ul>
+                    <div className="patient-claim-index-div">
+                        
+                        <div className="">
+                            Claim List
+                            <FaPlus className="" onClick={()=>window.location.replace(`#/patients/${patient.id}/claims/new`)}/>
+                        </div>
+
+                        <ul className="patient-claim-index-ul-category">
+                            <li className="patient-claim-index-ul-li-span1">DOS</li>
+                            <li className="patient-claim-index-ul-li-span2">Claim</li>
+                            <li className="patient-claim-index-ul-li-span3">Status</li>
+                        </ul>
+
+                        <ul className="patient-claim-index-ul">
+                            {claimList}
+                        </ul>
+                    </div>
+
                 </div>
                 
             </div>
